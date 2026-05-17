ec84602b65c008770439eb116736271839413262
Zorigo <zorigo.enkhtur@gmail.com>
2026-05-17 14:01:31 +0200

    bild

--- src/sections/main/About.jsx
+++ src/sections/main/About.jsx
@@ -1,4 +1,6 @@
-import "./About.scss";
+import React from "react";
+
+import aboutImg from "../../assets/about.jpg";
 
 function About() {
   return (
@@ -6,33 +8,8 @@ function About() {
       <div className="about__container">
         <div className="about__image-wrapper">
           <div className="about__glow-circle"></div>
-          <img
-            src="/src/assets/about.jpg"
-            alt="About Us"
-            className="about__img"
-          />
-        </div>
 
-        <div className="about__content">
-          <h2 className="about__title">
-            Mehr als nur <span className="highlight">Pixel.</span>
-          </h2>
-          <p className="about__text">
-            Bei <strong>Crazy Movies</strong> glauben wir, dass großartige
-            Geschichten nichts kosten sollten. Wir sind ein Kollektiv von
-            Film-Enthusiasten, die die besten Klassiker und Independent-Perlen
-            an einem Ort versammeln.
-          </p>
-          <div className="about__stats">
-            <div className="about__stat-item">
-              <span className="about__stat-number">500+</span>
-              <span className="about__stat-label">Filme</span>
-            </div>
-            <div className="about__stat-item">
-              <span className="about__stat-number">24/7</span>
-              <span className="about__stat-label">Streaming</span>
-            </div>
-          </div>
+          <img src={aboutImg} alt="About Us" className="about__img" />
         </div>
       </div>
     </section>
