ec444733633298a9606af5f13b90c869b788b1a3
Zorigo <zorigo.enkhtur@gmail.com>
2026-05-17 14:40:05 +0200

    bild

--- src/sections/main/About.jsx
+++ src/sections/main/About.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import aboutImg from "../../assets/about.jpg";
 
 function About() {
   return (
     <section className="about" id="about">
       <div className="about__container">
         <div className="about__image-wrapper">
-          <div className="about__glow-circle"></div>
-          <img src={aboutImg} alt="About Us" className="about__img" />
+          <img
+            src="/src/assets/about.jpg"
+            alt="About Us"
+            className="about__img"
+          />
         </div>
 
         <div className="about__content">
